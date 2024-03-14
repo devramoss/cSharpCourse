06a828553a575866996617d2547c28cfed1d0407
Emilly Ramos <71387482+devramoss@users.noreply.github.com>
2024-03-14 22:46:03 +0000

    Modificadores de parâmtros: ref e out

---
+++ ModificadoresParametros/Calculator.cs
@@ -0,0 +1,7 @@
+namespace ModificadoresParametros{
+   class Calculator{
+      public static void Triple(ref int origin){
+         origin = origin * 3;
+      }
+   }
+}
---
+++ ModificadoresParametros/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ModificadoresParametros{
+    internal class Program{
+            static void Main(string[] args){
+                int a = 10;
+                Calculator.Triple(ref a);
+                Console.WriteLine(a);
+             }
+    }
+
+}
